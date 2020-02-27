c409492fb6deac182309eb3b58d00e61e97886cc
murosan <m.murota.dev@gmail.com>
2020-02-27 20:21:31 +0900

    config_test

---
+++ gollect/config_test.go
@@ -0,0 +1,38 @@
+package gollect
+
+import (
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestLoadConfig(t *testing.T) {
+	base := filepath.Join(".", "testdata", "config")
+
+	want := &Config{
+		InputFile:   "abc/def/main.go",
+		OutputPaths: []string{"stdout", "clipboard"},
+	}
+	actual := LoadConfig(filepath.Join(base, "valid.yml"))
+
+	if !reflect.DeepEqual(want, actual) {
+		t.Errorf("\n[want]\n%v\n[actual]\n%v", want, actual)
+	}
+
+	shouldPanic(t, func() {
+		LoadConfig(filepath.Join(base, "invalid.yml"))
+	}, "should fail")
+}
+
+func TestConfig_Validate(t *testing.T) {
+	c1 := &Config{InputFile: "", OutputPaths: []string{"stdout"}}
+	c2 := &Config{InputFile: "main.go", OutputPaths: []string{"stdout"}}
+
+	if err := c1.Validate(); err == nil {
+		t.Error("want error for empty InputFile field but got nil")
+	}
+
+	if err := c2.Validate(); err != nil {
+		t.Errorf("want: nil, actual: %v", err)
+	}
+}
