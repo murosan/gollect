8fdaf166e51e9835a7e535dceb35cc0488c61cd4
murosan <m.murota.dev@gmail.com>
2020-02-27 19:35:51 +0900

    write_test

---
+++ gollect/writer_test.go
@@ -0,0 +1,46 @@
+package gollect
+
+import (
+	"testing"
+)
+
+func TestWriterProvider(t *testing.T) {
+	var provider writerProvider = &writerProviderImpl{}
+
+	for i, s := range []string{
+		"clipboard",
+		"CLIPBOARD",
+		"Clipboard",
+		"ClipBoard",
+	} {
+		switch v := provider.provide(s).(type) {
+		case *clipboardWriter:
+		// ok
+		default:
+			t.Errorf("at: %d, want: %s, actual: %T", i, "clipboardWriter", v)
+		}
+	}
+
+	for i, s := range []string{
+		"stdout",
+		"STDOUT",
+		"Stdout",
+		"StdOut",
+	} {
+		switch v := provider.provide(s).(type) {
+		case *stdoutWriter:
+		// ok
+		default:
+			t.Errorf("at: %d, want: %s, actual: %T", i, "stdoutWriter", v)
+		}
+	}
+
+	for i, s := range []string{"file", "abc"} {
+		switch v := provider.provide(s).(type) {
+		case *fileWriter:
+		// ok
+		default:
+			t.Errorf("at: %d, want: %s, actual: %T", i, "fileWriter", v)
+		}
+	}
+}
