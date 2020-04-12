8666295b4c7b44dfcd7d42dbae8fc4552929be8a
murosan <m.murota.dev@gmail.com>
2020-04-12 10:31:32 +0900

    print +/-

--- gollect_test.go
+++ gollect_test.go
@@ -8,9 +8,10 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
-	"github.com/sergi/go-diff/diffmatchpatch"
+	dmp "github.com/sergi/go-diff/diffmatchpatch"
 
 	"github.com/murosan/gollect/testdata"
 )
@@ -53,16 +54,39 @@ func TestGollect(t *testing.T) {
 			}
 
 			if !bytes.Equal(expected, actual) {
-				dmp := diffmatchpatch.New()
-				diff := dmp.DiffMain(string(expected), string(actual), false)
+				diff := dmp.New().DiffMain(string(expected), string(actual), true)
 				t.Errorf(`
 ===================================================================
 At: %d
 Diff:
 %s
 ===================================================================
-`, i, dmp.DiffPrettyText(diff))
+`, i, colorDiff(diff))
 			}
 		})
 	}
+}
+
+// https://github.com/sergi/go-diff/blob/master/diffmatchpatch/diff.go#L1183
+func colorDiff(diffs []dmp.Diff) string {
+	var b strings.Builder
+
+	for _, diff := range diffs {
+		text := diff.Text
+
+		switch diff.Type {
+		case dmp.DiffInsert:
+			_, _ = b.WriteString("\x1b[32m +")
+			_, _ = b.WriteString(text)
+			_, _ = b.WriteString("\x1b[0m")
+		case dmp.DiffDelete:
+			_, _ = b.WriteString("\x1b[31m -")
+			_, _ = b.WriteString(text)
+			_, _ = b.WriteString("\x1b[0m")
+		case dmp.DiffEqual:
+			_, _ = b.WriteString(text)
+		}
+	}
+
+	return b.String()
 }
